a29fcd8f564184dd61efcb66491e835ba1da9e92
arthur proto <arthu@10.3.62.87>
2025-10-24 11:40:44 +0200

    Tp0 hellowork

---
+++ TP0_ helloWorld/src/tp0_/helloworld/TP0_HelloWorld.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_.helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arthu
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println( " Bonjour " ) ;
+        System.out.println( " Au revoir  " ) ;
+String prenom;
+Scanner sc ;
+sc = new Scanner (System.in) ;
+System.out.println("Quel est votre prénom ?");
+prenom = sc.nextLine(); 
+        
+    }
+    
+}
+  
