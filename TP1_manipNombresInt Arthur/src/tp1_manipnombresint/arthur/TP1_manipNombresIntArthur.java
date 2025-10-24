b45bf0747db9de481881fd4e10e1e41ae2957650
arthur proto <arthu@10.3.62.87>
2025-10-24 12:10:21 +0200

    TP 1 nombre 24/10

--- TP1_manipNombresInt Arthur/src/tp1_manipnombresint/arthur/TP1_manipNombresIntArthur.java
+++ TP1_manipNombresInt Arthur/src/tp1_manipnombresint/arthur/TP1_manipNombresIntArthur.java
@@ -4,6 +4,8 @@
  */
 package tp1_manipnombresint.arthur;
 
+import java.util.Scanner;
+
 /**
  *
  * @author arthu
@@ -15,6 +17,25 @@ public class TP1_manipNombresIntArthur {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int nb1;int nb2;int somme;double division; int produit; int quotient;int reste ; 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer le nombre 1 :");
+        nb1=sc.nextInt();
+        System.out.println("Entrer le nombre2 :");
+        nb2=sc.nextInt();
+        somme = nb1 + nb2;
+        System.out.println("LA somme des deux nombres est "+somme);
+        division = nb1/nb2;
+        System.out.println("LA division du nb1 par nb2 est "+division);
+        produit = nb1*nb2;
+        System.out.println("Le produit des deux nombres est "+produit);
+        quotient = nb1 / nb2 ;
+        System.out.println("Le quotient des deux nombres est "+ quotient);
+        reste = nb1%nb2;
+        System.out.println("Le reste de la division des deux nombres est "+ reste);       
+
+                
+    
     }
     
 }
