8cf0c0167fcdeb857306fe595115a95ce637251b
arthur proto <arthu@10.3.62.87>
2025-11-01 19:48:23 +0100

    TP 0 end 1/11

--- TP0_calculette/src/tp0_calculette/TP0_calculette.java
+++ TP0_calculette/src/tp0_calculette/TP0_calculette.java
@@ -17,10 +17,48 @@ public class TP0_calculette {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int nb;
+
     Scanner sc = new Scanner(System.in);
   System.out.println("Please enter the operator: \n 1) add  \n 2) substract \n 3) multiply \n 4) divide \n 5) modulo");
-        nb=sc.nextInt();
+        
+        System.out.print("Votre choix (1-5) : ");
+        int operateur = sc.nextInt();
+
+        System.out.print("Entrez l'operande1 (entier) : ");
+        int operande1 = sc.nextInt();
+
+        System.out.print("Entrez l'operande2 (entier) : ");
+        int operande2 = sc.nextInt();
+
+        System.out.println(); // ligne vide pour la lisibilité
+
+        if (operateur == 1) {
+            System.out.println(operande1 + " + " + operande2 + " = " + (operande1 + operande2));
+
+        } else if (operateur == 2) {
+            System.out.println(operande1 + " - " + operande2 + " = " + (operande1 - operande2));
+
+        } else if (operateur == 3) {
+            System.out.println(operande1 + " * " + operande2 + " = " + (operande1 * operande2));
+
+        } else if (operateur == 4) {
+            if (operande2 == 0) {
+                System.out.println("Erreur : division par zéro impossible.");
+            } else {
+                double resultat = (double) operande1 / operande2;
+                System.out.println(operande1 + " / " + operande2 + " = " + resultat);
+            }
+
+        } else if (operateur == 5) {
+            if (operande2 == 0) {
+                System.out.println("Erreur : modulo par zéro impossible.");
+            } else {
+                System.out.println(operande1 + " % " + operande2 + " = " + (operande1 % operande2));
+            }
+
+        } else {
+            System.out.println("Choix invalide. Veuillez relancer et saisir un nombre entre 1 et 5.");
+        }
     }
     
 }
