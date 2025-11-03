056c18659ec83314c75518530214e2d3bd1d4c43
arthur proto <arthu@10.3.62.87>
2025-11-03 18:51:13 +0100

    tp 2 question 10

---
+++ TP2_Bieres_Proto/src/tp2_bieres_proto/TP2_Bieres_Proto.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_proto;
+
+/**
+ *
+ * @author arthu
+ */
+public class TP2_Bieres_Proto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+BouteilleBiere uneBiere = new BouteilleBiere() ;
+uneBiere.Nom = "Cuvée des trolls";
+uneBiere.degreAlcool = 7.0 ;
+uneBiere.brasserie = "Dubuisson";
+uneBiere.ouverte= false ; 
+uneBiere.lireEtiquette();
+uneBiere.Nom = "Leffe";
+uneBiere.degreAlcool = 6.6 ;
+uneBiere.brasserie = "Abbaye de Leffe";
+uneBiere.ouverte= false ; 
+uneBiere.lireEtiquette();
+
+    }
+    }
